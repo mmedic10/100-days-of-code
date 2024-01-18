4d157f8062fd90cfdef0ea520375045331e3f503
Marco Medic <medmatte@gmail.com>
2024-01-17 23:53:45 -0300

    primer dia completado

---
+++ day-001/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator.")
+city = input("What's the name of the city you gre up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be "+ city + " " + pet_name)
---
+++ day-001/main.py
@@ -0,0 +1,10 @@
+# Imprimir mensaje por consola
+print("Hola Mundo!\nHola Mundo")
+
+#Concatenar Strings
+print("Hola"+" "+"Marco")
+
+# Input funtions
+print("Hola "+input("¿Cual es tu nombre?"))
+
+
