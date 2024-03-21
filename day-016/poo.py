9fffa4d57f89f9a6870635172ac5ef77234dc498
mmedic10 <medmatte@gmail.com>
2024-03-20 23:31:35 -0300

    day 16

---
+++ day-016/poo.py
@@ -0,0 +1,26 @@
+# Poo
+
+# Programacion procedimental: Un procedimiento lleva a otro procedimiento, en general el computar trabaja desde arriba hacia abajo y luego de una funcion a otra
+# La programacion procedimental es uno de los primeros paradigmas de la programacion
+# Mientras más aumente el codigo más complejo se hacen las llamadas y procedimientos
+
+# POO = Programacion orientada de objetos. Es uno de los paradigmas de la programacion.
+
+# Las dos cosas más importantes que componen un objeto es: sus atributos y sus metodos
+# Un atributo es una variable que esta asociada a un objeto modelado
+# Los metodos son funciones asociadas a un objeto modelado y se les llama asi porque es una funcion que un determinado modelo puede hacer
+
+# Se denomina clase al plano en general del modelo y objeto a todos los objetos individuales que se generan por los planos
+# Una clase va con mayuscula cada letra de cada palabra(esto se denomica caso Pascal) para diferenciarla de todas las variables y funciones.
+
+#import another_module
+#print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+#timmy = Turtle()
+
+#print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
